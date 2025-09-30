852177f0e3f16cf94c2edbab2a6af35a11ffef9f
deybid castillo <deybid.castillo@upb.edu.co>
2025-09-30 11:05:56 -0500

    terminacion ej 1

---
+++ src/listas/ejercicio 1.py
@@ -0,0 +1,23 @@
+'''
+componentes = []
+elemento = input("ingrese un componente del avion: ")
+componentes.append(elemento)
+print(componentes)
+'''
+'''
+import random
+numeros = []
+numeros.append(10)
+for i in range(10):
+    aleatorio = random.randint(0,100)
+    numeros.append(aleatorio)
+print(numeros)
+'''
+
+'''
+componentes = ["alas", "fuselaje","motores", "tren de aterrizaje"]
+
+datos_vuelo = [202, "boeing 737", True, 10500.5]
+
+matriz_rotacion = [[1,0,0],[0,1,0],[0,0,1]]
+'''
